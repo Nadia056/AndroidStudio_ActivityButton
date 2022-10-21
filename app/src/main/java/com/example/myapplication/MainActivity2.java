60d436fc6e6fedf61a6bc04c302fcd7bb9992840
nadia356 <110473332+nadia356@users.noreply.github.com>
2022-10-21 00:39:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+       Button btn1=findViewById(R.id.btn1);
+       btn1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i=new Intent(MainActivity2.this,MainActivity.class);
+               startActivity(i);
+
+           }
+       });
+    }
+
+
+
+}
